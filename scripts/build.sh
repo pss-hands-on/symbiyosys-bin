bd94f15472d72f1f7e0d776e82ca2447ebc0ce96
Matthew Ballance <matt.ballance@gmail.com>
2025-01-01 02:23:14 +0000

    Initial CI
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ PATH_SAV=${PATH}
 if test "x${CI_BUILD}" != "x"; then
     if test $(uname -s) = "Linux"; then
         yum update -y
-        yum install -y glibc-static wget flex bison jq
+        yum install -y glibc-static wget flex bison jq readline-static
         export PATH=/opt/python/cp312-cp312/bin:$PATH
         rls_plat="manylinux-x64"
     elif test $(uname -s) = "Windows"; then
